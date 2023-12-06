e28a6521050a5ca6e45e96a4deae6c5a2a325cd8
tangbin <yvhk2900@gmail.com>
2023-12-06 17:17:23 +0800

    update

--- src/main/java/com/anzhilai/core/toolkit/image/VerifyImageUtil.java
+++ src/main/java/com/anzhilai/core/toolkit/image/VerifyImageUtil.java
@@ -17,21 +17,30 @@ import java.util.Random;
  * 滑块图片验证
  */
 public class VerifyImageUtil {
-    //    private static int CUT_WIDTH = 50;//模板图宽度
-//    private static int CUT_HEIGHT = 50;//模板图高度
-//    private static int circleR = 5;//抠图凸起圆心
-//    private static int RECTANGLE_PADDING = 8;//抠图内部矩形填充大小
-//    private static int SLIDER_IMG_OUT_PADDING = 1;//抠图的边框宽度
-    private static int CUT_WIDTH = 100;//模板图宽度
-    private static int CUT_HEIGHT = 100;//模板图高度
-    private static int circleR = 10;//抠图凸起圆心
-    private static int RECTANGLE_PADDING = 18;//抠图内部矩形填充大小
-    private static int SLIDER_IMG_OUT_PADDING = 2;//抠图的边框宽度
+    private int cutWidth = 100;//模板图宽度
+    private int cutHeight = 100;//模板图高度
+    private int circleR = 10;//抠图凸起圆心
+    private int rectanglePadding = 18;//抠图内部矩形填充大小
+    private int sliderImgOutPadding = 2;//抠图的边框宽度
+
+    public VerifyImageUtil() {
+        this(1);
+    }
+
+    public VerifyImageUtil(double cutZoom) {
+        if (cutZoom > 0 && cutZoom != 1) {
+            this.cutWidth = (int) (cutWidth * cutZoom);
+            this.cutHeight = (int) (cutHeight * cutZoom);
+            this.circleR = (int) (circleR * cutZoom);
+            this.rectanglePadding = (int) (rectanglePadding * cutZoom);
+            this.sliderImgOutPadding = (int) (sliderImgOutPadding * cutZoom);
+        }
+    }
 
     public static void main(String[] args) {
         try {
             String basePath = "C:\\Users\\tangbin\\Desktop\\";
-            VerifyImage img = VerifyImageUtil.getVerifyImage(basePath + "test.jpg", basePath + "1.png", basePath + "2.png");
+            VerifyImage img = new VerifyImageUtil().getVerifyImage(basePath + "test.jpg", basePath + "1.png", basePath + "2.png");
             System.out.println("x,y:" + img.XPosition + "," + img.YPosition);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -42,18 +51,18 @@ public class VerifyImageUtil {
     /**
      * 根据传入的路径生成指定验证码图片
      */
-    public static VerifyImage getVerifyImage(String filePath) throws IOException {
+    public VerifyImage getVerifyImage(String filePath) throws IOException {
         return getVerifyImage(filePath, null, null);
     }
 
     /**
      * 根据传入的路径生成指定验证码图片
      */
-    public static VerifyImage getVerifyImage(String filePath, String imgPath, String makeImagPath) throws IOException {
+    public VerifyImage getVerifyImage(String filePath, String imgPath, String makeImagPath) throws IOException {
         BufferedImage srcImage = ImageIO.read(new File(filePath));
-        int locationX = CUT_WIDTH + new Random().nextInt(srcImage.getWidth() - CUT_WIDTH * 3);
-        int locationY = CUT_HEIGHT + new Random().nextInt(srcImage.getHeight() - CUT_HEIGHT) / 2;
-        BufferedImage markImage = new BufferedImage(CUT_WIDTH, CUT_HEIGHT, BufferedImage.TYPE_4BYTE_ABGR);
+        int locationX = cutWidth + new Random().nextInt(srcImage.getWidth() - cutWidth * 3);
+        int locationY = cutHeight + new Random().nextInt(srcImage.getHeight() - cutHeight) / 2;
+        BufferedImage markImage = new BufferedImage(cutWidth, cutHeight, BufferedImage.TYPE_4BYTE_ABGR);
         int[][] data = getBlockData();
         cutImgByTemplate(srcImage, markImage, data, locationX, locationY);
         if (StrUtil.isNotEmpty(imgPath)) {
@@ -74,7 +83,7 @@ public class VerifyImageUtil {
      *
      * @param oriImage 图片
      */
-    private static BufferedImage addWatermark(BufferedImage oriImage) {
+    private BufferedImage addWatermark(BufferedImage oriImage) {
         Graphics2D graphics2D = oriImage.createGraphics();
         graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         // 设置水印文字颜色
@@ -99,29 +108,29 @@ public class VerifyImageUtil {
      *
      * @return int[][]
      */
-    private static int[][] getBlockData() {
-        int[][] data = new int[CUT_WIDTH][CUT_HEIGHT];
+    private int[][] getBlockData() {
+        int[][] data = new int[cutWidth][cutHeight];
         Random random = new Random();
         //(x-a)²+(y-b)²=r²
         //x中心位置左右5像素随机
-        double x1 = RECTANGLE_PADDING + (CUT_WIDTH - 2 * RECTANGLE_PADDING) / 2.0 - 5 + random.nextInt(10);
+        double x1 = rectanglePadding + (cutWidth - 2 * rectanglePadding) / 2.0 - 5 + random.nextInt(10);
         //y 矩形上边界半径-1像素移动
-        double y1_top = RECTANGLE_PADDING - random.nextInt(3);
-        double y1_bottom = CUT_HEIGHT - RECTANGLE_PADDING + random.nextInt(3);
+        double y1_top = rectanglePadding - random.nextInt(3);
+        double y1_bottom = cutHeight - rectanglePadding + random.nextInt(3);
         double y1 = random.nextInt(2) == 1 ? y1_top : y1_bottom;
 
-        double x2_right = CUT_WIDTH - RECTANGLE_PADDING - circleR + random.nextInt(2 * circleR - 4);
-        double x2_left = RECTANGLE_PADDING + circleR - 2 - random.nextInt(2 * circleR - 4);
+        double x2_right = cutWidth - rectanglePadding - circleR + random.nextInt(2 * circleR - 4);
+        double x2_left = rectanglePadding + circleR - 2 - random.nextInt(2 * circleR - 4);
         double x2 = random.nextInt(2) == 1 ? x2_right : x2_left;
-        double y2 = RECTANGLE_PADDING + (CUT_HEIGHT - 2 * RECTANGLE_PADDING) / 2.0 - 4 + random.nextInt(10);
+        double y2 = rectanglePadding + (cutHeight - 2 * rectanglePadding) / 2.0 - 4 + random.nextInt(10);
 
         double po = Math.pow(circleR, 2);
-        for (int i = 0; i < CUT_WIDTH; i++) {
-            for (int j = 0; j < CUT_HEIGHT; j++) {
+        for (int i = 0; i < cutWidth; i++) {
+            for (int j = 0; j < cutHeight; j++) {
                 //矩形区域
                 boolean fill;
-                if ((i >= RECTANGLE_PADDING && i < CUT_WIDTH - RECTANGLE_PADDING)
-                        && (j >= RECTANGLE_PADDING && j < CUT_HEIGHT - RECTANGLE_PADDING)) {
+                if ((i >= rectanglePadding && i < cutWidth - rectanglePadding)
+                        && (j >= rectanglePadding && j < cutHeight - rectanglePadding)) {
                     data[i][j] = 1;
                     fill = true;
                 } else {
@@ -145,37 +154,37 @@ public class VerifyImageUtil {
             }
         }
         //边界阴影
-        for (int i = 0; i < CUT_WIDTH; i++) {
-            for (int j = 0; j < CUT_HEIGHT; j++) {
+        for (int i = 0; i < cutWidth; i++) {
+            for (int j = 0; j < cutHeight; j++) {
                 //四个正方形边角处理
-                for (int k = 1; k <= SLIDER_IMG_OUT_PADDING; k++) {
+                for (int k = 1; k <= sliderImgOutPadding; k++) {
                     //左上、右上
-                    if (i >= RECTANGLE_PADDING - k && i < RECTANGLE_PADDING
-                            && ((j >= RECTANGLE_PADDING - k && j < RECTANGLE_PADDING)
-                            || (j >= CUT_HEIGHT - RECTANGLE_PADDING - k && j < CUT_HEIGHT - RECTANGLE_PADDING + 1))) {
+                    if (i >= rectanglePadding - k && i < rectanglePadding
+                            && ((j >= rectanglePadding - k && j < rectanglePadding)
+                            || (j >= cutHeight - rectanglePadding - k && j < cutHeight - rectanglePadding + 1))) {
                         data[i][j] = 2;
                     }
                     //左下、右下
-                    if (i >= CUT_WIDTH - RECTANGLE_PADDING + k - 1 && i < CUT_WIDTH - RECTANGLE_PADDING + 1) {
-                        for (int n = 1; n <= SLIDER_IMG_OUT_PADDING; n++) {
-                            if (((j >= RECTANGLE_PADDING - n && j < RECTANGLE_PADDING)
-                                    || (j >= CUT_HEIGHT - RECTANGLE_PADDING - n && j <= CUT_HEIGHT - RECTANGLE_PADDING))) {
+                    if (i >= cutWidth - rectanglePadding + k - 1 && i < cutWidth - rectanglePadding + 1) {
+                        for (int n = 1; n <= sliderImgOutPadding; n++) {
+                            if (((j >= rectanglePadding - n && j < rectanglePadding)
+                                    || (j >= cutHeight - rectanglePadding - n && j <= cutHeight - rectanglePadding))) {
                                 data[i][j] = 2;
                             }
                         }
                     }
                 }
-                if (data[i][j] == 1 && j - SLIDER_IMG_OUT_PADDING > 0 && data[i][j - SLIDER_IMG_OUT_PADDING] == 0) {
-                    data[i][j - SLIDER_IMG_OUT_PADDING] = 2;
+                if (data[i][j] == 1 && j - sliderImgOutPadding > 0 && data[i][j - sliderImgOutPadding] == 0) {
+                    data[i][j - sliderImgOutPadding] = 2;
                 }
-                if (data[i][j] == 1 && j + SLIDER_IMG_OUT_PADDING > 0 && j + SLIDER_IMG_OUT_PADDING < CUT_HEIGHT && data[i][j + SLIDER_IMG_OUT_PADDING] == 0) {
-                    data[i][j + SLIDER_IMG_OUT_PADDING] = 2;
+                if (data[i][j] == 1 && j + sliderImgOutPadding > 0 && j + sliderImgOutPadding < cutHeight && data[i][j + sliderImgOutPadding] == 0) {
+                    data[i][j + sliderImgOutPadding] = 2;
                 }
-                if (data[i][j] == 1 && i - SLIDER_IMG_OUT_PADDING > 0 && data[i - SLIDER_IMG_OUT_PADDING][j] == 0) {
-                    data[i - SLIDER_IMG_OUT_PADDING][j] = 2;
+                if (data[i][j] == 1 && i - sliderImgOutPadding > 0 && data[i - sliderImgOutPadding][j] == 0) {
+                    data[i - sliderImgOutPadding][j] = 2;
                 }
-                if (data[i][j] == 1 && i + SLIDER_IMG_OUT_PADDING > 0 && i + SLIDER_IMG_OUT_PADDING < CUT_WIDTH && data[i + SLIDER_IMG_OUT_PADDING][j] == 0) {
-                    data[i + SLIDER_IMG_OUT_PADDING][j] = 2;
+                if (data[i][j] == 1 && i + sliderImgOutPadding > 0 && i + sliderImgOutPadding < cutWidth && data[i + sliderImgOutPadding][j] == 0) {
+                    data[i + sliderImgOutPadding][j] = 2;
                 }
             }
         }
@@ -192,13 +201,13 @@ public class VerifyImageUtil {
      * @param x           裁剪点x
      * @param y           裁剪点y
      */
-    private static void cutImgByTemplate(BufferedImage oriImage, BufferedImage targetImage, int[][] blockImage, int x, int y) {
+    private void cutImgByTemplate(BufferedImage oriImage, BufferedImage targetImage, int[][] blockImage, int x, int y) {
         // 临时数组遍历用于高斯模糊存周边像素值
         int[][] martrix = new int[3][3];
         int[] values = new int[9];
 
-        for (int i = 0; i < CUT_WIDTH; i++) {
-            for (int j = 0; j < CUT_HEIGHT; j++) {
+        for (int i = 0; i < cutWidth; i++) {
+            for (int j = 0; j < cutHeight; j++) {
                 int _x = x + i;
                 int _y = y + j;
                 int rgbFlg = blockImage[i][j];
@@ -227,7 +236,7 @@ public class VerifyImageUtil {
         }
     }
 
-    private static void readPixel(BufferedImage img, int x, int y, int[] pixels) {
+    private void readPixel(BufferedImage img, int x, int y, int[] pixels) {
         int xStart = x - 1;
         int yStart = y - 1;
         int current = 0;
@@ -251,7 +260,7 @@ public class VerifyImageUtil {
             }
     }
 
-    private static void fillMatrix(int[][] matrix, int[] values) {
+    private void fillMatrix(int[][] matrix, int[] values) {
         int filled = 0;
         for (int i = 0; i < matrix.length; i++) {
             int[] x = matrix[i];
@@ -262,7 +271,7 @@ public class VerifyImageUtil {
     }
 
 
-    private static int avgMatrix(int[][] matrix) {
+    private int avgMatrix(int[][] matrix) {
         int r = 0;
         int g = 0;
         int b = 0;
@@ -289,7 +298,7 @@ public class VerifyImageUtil {
      * @return
      * @throws IOException
      */
-    public static BufferedImage getRandomImage(String path) throws IOException {
+    public BufferedImage getRandomImage(String path) throws IOException {
         File files = new File(path);
         File[] fileList = files.listFiles();
         List<String> fileNameList = new ArrayList<>();
@@ -312,7 +321,7 @@ public class VerifyImageUtil {
      * @param file
      * @throws Exception
      */
-    public static void writeImg(BufferedImage image, String file) throws Exception {
+    public void writeImg(BufferedImage image, String file) throws Exception {
         byte[] imagedata = null;
         ByteArrayOutputStream bao = new ByteArrayOutputStream();
         ImageIO.write(image, "png", bao);
@@ -329,7 +338,7 @@ public class VerifyImageUtil {
      * @return
      * @throws IOException
      */
-    public static String getImageBASE64(BufferedImage image, boolean compress) throws IOException {
+    public String getImageBASE64(BufferedImage image, boolean compress) throws IOException {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         if (compress) {
             Thumbnails.of(image).size(image.getWidth(), image.getHeight()).outputQuality(0.5)
@@ -346,7 +355,7 @@ public class VerifyImageUtil {
      * @param base64String
      * @return
      */
-    public static BufferedImage base64StringToImage(String base64String) {
+    public BufferedImage base64StringToImage(String base64String) {
         try {
             Base64.Decoder decoder = Base64.getDecoder();
             byte[] bytes1 = decoder.decode(base64String);
